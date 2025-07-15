f7c94916f2d2ffa220d26367eb985dc02b390aa6
Ricardocampos95 <ricardo.eteo@gmail.com>
2025-07-15 23:35:46 +0100

    H2 database, test profile, JPA

--- src/main/java/com/campostech/course/resources/UserResource.java
+++ src/main/java/com/campostech/course/resources/UserResource.java
@@ -14,7 +14,10 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "918390933", "12345");
+	
 		return ResponseEntity.ok().body(u);
 	}
+	
+
 
 }
